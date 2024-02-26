8f64115e0acf949db79c55d4e0a90d31bdfd56ca
bellinzona <mtbellinzona@gmail.com>
2024-02-26 15:31:30 -0300

    cambio de direcciones 2.1

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.post("/create_preference", (req, res) => {
       }
     ],
     back_urls: {
-      "success": `http://localhost:5173/CompraFinalizada?title=${req.body.description}&unit_price=${req.body.price}&quantity=${req.body.quantity}`,
+      "success": `https://deluxe-jalebi-133777.netlify.app/CompraFinalizada?title=${req.body.description}&unit_price=${req.body.price}&quantity=${req.body.quantity}`,
       "failure": "https://deluxe-jalebi-133777.netlify.app/",
       "pending": ""
     },
