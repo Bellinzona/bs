778bbfe68c4447edbfe73b5a487d17a1c3b49940
bellinzona <mtbellinzona@gmail.com>
2024-06-19 14:54:42 -0300

    express static

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const mercadopago = require("mercadopago");
-const db = require("./firebaseConfig")
+const path = require("path");
+const db = require("./firebaseConfig");
 
+const app = express();
 
 mercadopago.configure({
   access_token: "APP_USR-4072506350930351-122920-1476b0073389bef23ff181ab511922e7-1070363034",
@@ -15,8 +16,8 @@ app.use(cors());
 // Variable global en el objeto app
 app.set('globalInfo', {});
 
+// Endpoints de la API
 app.post("/create_preference", (req, res) => {
-  // Crear un objeto con la información del req.body.description
   const requestBodyInfo = {
     description: req.body.description,
     price: Number(req.body.price),
@@ -28,9 +29,8 @@ app.post("/create_preference", (req, res) => {
     local: req.body.local
   };
 
-  console.log(requestBodyInfo)
+  console.log(requestBodyInfo);
 
-  // Guardar la información en el objeto app
   app.set('globalInfo', requestBodyInfo);
 
   let preference = {
@@ -40,8 +40,6 @@ app.post("/create_preference", (req, res) => {
         unit_price: requestBodyInfo.price,
         quantity: Number(req.body.quantity),
       }
-
-      
     ],
     back_urls: {
       "success": `https://bubbamilanesas.com/`,
@@ -50,14 +48,13 @@ app.post("/create_preference", (req, res) => {
     },
     auto_return: "approved",
     notification_url: "https://bs-i4ni.onrender.com/webHook"
-    
   };
 
   mercadopago.preferences.create(preference)
     .then(function (response) {
       res.json({
         id: response.body.id,
-        RequestBodyInfo: app.get('globalInfo') // Obtener la información del objeto app
+        RequestBodyInfo: app.get('globalInfo')
       });
     })
     .catch(function (error) {
@@ -69,63 +66,55 @@ app.post("/webHook", (req, res) => {
   const payment = req.body;
   const RequestBodyInfo = app.get('globalInfo');
 
-  // Enviar respuesta HTTP 200 inmediatamente para confirmar recepción
   res.sendStatus(200);
 
   if (payment.action === 'payment.created') {
     console.log(`Payment created: ${JSON.stringify(payment)}`);
 
     db.collection("Pagos").add(RequestBodyInfo)
-    .then(docRef => {
-      console.log("Document written with ID: ", docRef.id);
-      console.log("esto se tiene que guardar: ", RequestBodyInfo);
-
-    })
-    .catch(error => {
-      console.error("Error adding document: ", error);
-    });
-
-
-
+      .then(docRef => {
+        console.log("Document written with ID: ", docRef.id);
+        console.log("esto se tiene que guardar: ", RequestBodyInfo);
+      })
+      .catch(error => {
+        console.error("Error adding document: ", error);
+      });
   } else if (payment.action === 'payment.updated') {
     console.log(`Payment updated: ${JSON.stringify(payment)}`);
 
     db.collection("Pagos").add(RequestBodyInfo)
-    .then(docRef => {
-      console.log("esto se tiene que guardar: ", RequestBodyInfo);
-      console.log("Document written with ID: ", docRef.id);
-
-    })
-    .catch(error => {
-      console.error("Error adding document: ", error);
-    });
+      .then(docRef => {
+        console.log("esto se tiene que guardar: ", RequestBodyInfo);
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(error => {
+        console.error("Error adding document: ", error);
+      });
   }
-
-  
-
-  
 });
 
 app.get('/CompraFinalizada', (req, res) => {
-  // Puedes acceder a todos los parámetros de la URL a través de req.query
   const requestBodyInfo = req.query;
-
-  // Obtén la información del objeto app
   const appInfo = app.get('globalInfo');
-
-  // Agrega requestBodyInfo al objeto appInfo
   const responseInfo = {
     RequestBodyInfo: appInfo,
     QueryParams: requestBodyInfo
   };
 
-  // Aquí deberías tener la lógica para obtener la información del pago, por ejemplo, consultando una base de datos
-  // Puedes ajustar esta parte según tus necesidades
-
-  console.log(responseInfo)
+  console.log(responseInfo);
   res.json(responseInfo);
 });
 
-app.listen(8080, () => {
-  console.log("The server is now running on Port 8080");
+// Servir archivos estáticos de React desde la carpeta client/build
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Servir index.html para todas las rutas no manejadas explícitamente
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
+
+// Iniciar el servidor
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`The server is now running on Port ${port}`);
 });
